c695c283efccae9a2291479d47d7bbf777613cb2
Planeshifter <pburckhardt@outlook.com>
2015-11-01 05:34:17 -0500

    [UPDATE] fun visualization

---
+++ slides/script.js
@@ -0,0 +1,10 @@
+global.rNorm = require( 'distributions-normal-random' );
+global.pdfNorm = require( 'distributions-normal-pdf' );
+global.qNorm = require( 'distributions-normal-quantile' );
+global.cdfNorm = require( 'distributions-normal-cdf' );
+global.mean = require( 'compute-mean' );
+global.matrix = require( 'dstructs-matrix' );
+global.linspace = require( 'compute-linspace' );
+global.incrspace = require( 'compute-incrspace' );
+global.logspace = require( 'compute-logspace' );
+global.indexspace = require( 'compute-indexspace' );
