2eded4b506b566a272e22abb91b4aad4f835cf13
Planeshifter <pburckhardt@outlook.com>
2015-11-05 13:43:00 -0500

    [FIX] move attach globals to script.js

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
 global.distributions = require( 'distributions.io' );
 global.compute = require( 'compute.io' );
-global.matrix = require( 'dstructs-matrix' ); 
+global.matrix = require( 'dstructs-matrix' );
+
+createGlobals( compute );
+createGlobals( distributions );
+
+/**
+* FUNCTION: createGlobals()
+*	Binds all object properties to the global `window` object.
+*/
+function createGlobals( obj ) {
+	var keys,
+		i;
+	keys = Object.keys( obj );
+	for ( i = 0; i < keys.length; i++ ) {
+		window[ keys[i] ] = obj[ keys[i] ];
+	}
+} // end FUNCTION createGlobals()
